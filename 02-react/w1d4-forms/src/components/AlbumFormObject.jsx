b2564f896fa8a26d3b1c8f9d15dbfa4e9aa0762b
narcisolobo <narcisolobo@gmail.com>
2023-04-06 14:46:13 -0700

    state object version with validation onblur

--- 02-react/w1d4-forms/src/components/AlbumFormObject.jsx
+++ 02-react/w1d4-forms/src/components/AlbumFormObject.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 
 function AlbumFormObject() {
   const [album, setAlbum] = useState({
-    albumTitle: '',
-    albumArtist: '',
-    trackNumber: '',
+    title: '',
+    artist: '',
+    tracks: '',
+  });
+
+  const [albumErrors, setAlbumErrors] = useState({
+    title: '',
+    artist: '',
+    tracks: '',
   });
 
   const handleChange = (e) => {
@@ -16,8 +22,53 @@ function AlbumFormObject() {
     });
   };
 
+  const handleBlur = (e) => {
+    let error = '';
+    switch (e.target.name) {
+      case 'title':
+        if (e.target.value.trim().length < 2) {
+          error = 'Title must be at least two characters.';
+        } else {
+          error = null;
+        }
+        break;
+      case 'artist':
+        if (e.target.value.trim().length < 2) {
+          error = 'Artist must be at least two characters.';
+        } else {
+          error = null;
+        }
+        break;
+      case 'tracks':
+        if (Number.isNaN(e.target.value)) {
+          error = 'Please enter a number.';
+        } else {
+          error = null;
+        }
+        break;
+      default:
+        console.error('Unexpected target name.');
+        break;
+    }
+    setAlbumErrors({
+      ...albumErrors,
+      [e.target.name]: error,
+    });
+  };
+
+  let formIsValid =
+    albumErrors.title === null &&
+    albumErrors.artist === null &&
+    albumErrors.tracks === null;
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(album);
+    setAlbum({
+      title: '',
+      artist: '',
+      tracks: '',
+    });
   };
 
   return (
@@ -28,41 +79,61 @@ function AlbumFormObject() {
             {/* controlled input */}
             <input
               type="text"
-              name="albumTitle"
-              id="albumTitle"
+              name="title"
+              id="title"
               className="form-control"
               placeholder="Album Title"
-              value={album.albumTitle}
+              value={album.title}
               onChange={handleChange}
+              onBlur={handleBlur}
             />
-            <label htmlFor="albumTitle">Album Title:</label>
+            <label htmlFor="title">Album Title:</label>
+            {albumErrors.title && (
+              <span className="form-text text-danger">{albumErrors.title}</span>
+            )}
           </div>
           <div className="form-floating mb-3">
             <input
               type="text"
-              name="albumArtist"
-              id="albumArtist"
+              name="artist"
+              id="artist"
               className="form-control"
               placeholder="Album Artist"
-              value={album.albumArtist}
+              value={album.artist}
               onChange={handleChange}
+              onBlur={handleBlur}
             />
-            <label htmlFor="albumArtist">Album Artist:</label>
+            <label htmlFor="artist">Album Artist:</label>
+            {albumErrors.artist && (
+              <span className="form-text text-danger">
+                {albumErrors.artist}
+              </span>
+            )}
           </div>
           <div className="form-floating mb-3">
             <input
               type="number"
-              name="trackNumber"
-              id="trackNumber"
+              name="tracks"
+              id="tracks"
               className="form-control"
               placeholder="Track Number"
-              value={album.trackNumber}
+              value={album.tracks}
               onChange={handleChange}
+              onBlur={handleBlur}
             />
-            <label htmlFor="trackNumber">Track Number:</label>
+            <label htmlFor="tracks">Track Number:</label>
+            {albumErrors.tracks && (
+              <span className="form-text text-danger">
+                {albumErrors.tracks}
+              </span>
+            )}
           </div>
           <div className="text-end">
-            <button type="submit" className={`btn btn-outline-success`}>
+            <button
+              type="submit"
+              className={`btn btn-outline-success ${
+                formIsValid ? '' : 'disabled'
+              }`}>
               Add Album
             </button>
           </div>
